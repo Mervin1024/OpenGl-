5c464f1551a19a142cf49bebf01bb009495186c7
马遥 <mervin1024@163.com>
2018-03-03 18:04:15 +0800

    补完绘制三角形并旋转的练习代码

--- OpenGl基础环境/001-绘制三角形并移动/main.cpp
+++ OpenGl基础环境/001-绘制三角形并移动/main.cpp
@@ -4,11 +4,24 @@
 
 #include <glut/glut.h>
 
+// 批次容器，GLTools的一个容器类
 GLBatch triangleBatch;
 
+// 着色管理器
 GLShaderManager shaderManager;
 
-//窗口大小改变时接受新的宽度和高度，其中0,0代表窗口中视口的左下角坐标，w，h代表像素
+// 三角形顶点数据
+GLfloat halfWidth = 0.1f;
+GLfloat vVerts[] = {
+    0.0f, 2*halfWidth, 0.0f,
+    -halfWidth, 0.0f, 0.0f,
+    halfWidth, 0.0f, 0.0f
+};
+
+
+GLfloat xPos = 0.0f, yPos = 0.0f;
+
+#pragma mark - 窗口大小改变时接受新的宽度和高度，其中0,0代表窗口中视口的左下角坐标，w，h代表像素
 
 void ChangeSize(int w,int h)
 
@@ -18,7 +31,7 @@ void ChangeSize(int w,int h)
     
 }
 
-//为程序作一次性的设置
+#pragma mark - 为程序作一次性的设置
 
 void SetupRC()
 
@@ -26,7 +39,7 @@ void SetupRC()
     
     //设置背影颜色
     
-    glClearColor(0.0f,0.0f,1.0f,1.0f);
+    glClearColor(1.0f,1.0f,1.0f,1.0f);
     
     //初始化着色管理器
     
@@ -34,15 +47,15 @@ void SetupRC()
     
     //设置三角形，其中数组vVert包含所有3个顶点的x,y,笛卡尔坐标对。
     
-    GLfloat vVerts[] = {
-        
-        -0.5f,0.0f,0.0f,
-        
-        0.5f,0.0f,0.0f,
-        
-        0.0f,0.5f,0.0f,
-        
-    };
+//    GLfloat vVerts[] = {
+//
+//        -0.5f,0.0f,0.0f,
+//
+//        0.5f,0.0f,0.0f,
+//
+//        0.0f,0.5f,0.0f,
+//
+//    };
     
     //批次处理
     
@@ -54,7 +67,7 @@ void SetupRC()
     
 }
 
-//开始渲染
+#pragma mark - 开始渲染
 
 void RenderScene(void)
 
@@ -64,13 +77,40 @@ void RenderScene(void)
     
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
     
-    //设置一组浮点数来表示红色
+    //设置一组浮点数来表示黑色
     
-    GLfloat vRed[] = {1.0f,0.0f,0.0f,1.0f};
+    GLfloat vBlack[] = {0.0f,0.0f,0.0f,1.0f};
     
     //传递到存储着色器，即GLT_SHADER_IDENTITY着色器，这个着色器只是使用指定颜色以默认笛卡尔坐标第在屏幕上渲染几何图形
     
-    shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vRed);
+//    shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vBlack); // 通过坐标方式绘制图形
+    
+    // 利用矩阵绘制图形，需要用到平面着色器
+    M3DMatrix44f mFinalTransform, mTransFormMatrix, mRotationMatrix;
+    
+    m3dTranslationMatrix44(mTransFormMatrix, xPos, yPos, 0.0f);
+    
+//    shaderManager.UseStockShader(GLT_SHADER_FLAT,mTransFormMatrix,vBlack);
+    //增加难度! 一边移动，一边旋转
+    static float yRot = 0.0f;
+
+    /*
+     参数1：矩阵
+     参数2：弧度
+     参数3：X:1，0 1：围绕X轴旋转，0不围绕X轴旋转
+     参数4：Y:1，0
+     参数5：Z:1，0
+     */
+    m3dRotationMatrix44(mRotationMatrix, m3dDegToRad(yRot), 0.0f, 0.0f, 1.0f);
+
+    //修改旋转度数
+    yRot += 5.0f;
+
+
+
+    m3dMatrixMultiply44(mFinalTransform, mTransFormMatrix, mRotationMatrix);
+
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, mFinalTransform, vBlack);
     
     //提交着色器
     
@@ -82,6 +122,80 @@ void RenderScene(void)
     
 }
 
+#pragma mark - 键盘输入操作(移动图形---通过修改图形坐标)
+//void SpeacialKeysRect(int key, int x, int y) {
+//    GLfloat stepSize = 0.025f;
+//
+//    GLfloat originX = vVerts[3];
+//    GLfloat originY = vVerts[1];
+//    
+//    if (key == GLUT_KEY_UP) {
+//        originY += stepSize;
+//    } else if (key == GLUT_KEY_DOWN) {
+//        originY -= stepSize;
+//    } else if (key == GLUT_KEY_LEFT) {
+//        originX -= stepSize;
+//    } else if (key == GLUT_KEY_RIGHT) {
+//        originX += stepSize;
+//    }
+//
+//    // 加入边界判断
+//    if (originX < -1.0f) {
+//        originX = -1.0f;
+//    }
+//    if (originX > 1.0f - halfWidth*2) {
+//        originX = 1.0f - halfWidth*2;
+//    }
+//    if (originY > 1.0f) {
+//        originY = 1.0f;
+//    }
+//    if (originY < -1.0f + halfWidth*2) {
+//        originY = -1.0f + halfWidth*2;
+//    }
+//
+//    vVerts[0] = originX + halfWidth;
+//    vVerts[1] = originY;
+//    vVerts[3] = originX;
+//    vVerts[4] = originY - 2*halfWidth;
+//    vVerts[6] = originX + 2*halfWidth;
+//    vVerts[7] = vVerts[4];
+//
+//    triangleBatch.CopyVertexData3f(vVerts);
+//    glutPostRedisplay();
+//}
+
+#pragma mark - 键盘输入操作(移动图形---通过矩阵方式)
+void SpeacialKeysMatrix(int key, int x, int y) {
+    GLfloat stepSize = 0.025f;
+    
+    if (key == GLUT_KEY_UP) {
+        yPos += stepSize;
+    } else if (key == GLUT_KEY_DOWN) {
+        yPos -= stepSize;
+    } else if (key == GLUT_KEY_LEFT) {
+        xPos -= stepSize;
+    } else if (key == GLUT_KEY_RIGHT) {
+        xPos += stepSize;
+    }
+    
+    // 加入边界判断
+    if (xPos < -1.0f + halfWidth) {
+        xPos = -1.0f + halfWidth;
+    }
+    if (xPos > 1.0f - halfWidth) {
+        xPos = 1.0f - halfWidth;
+    }
+    if (yPos > 1.0f - 2*halfWidth) {
+        yPos = 1.0f - 2*halfWidth;
+    }
+    if (yPos < -1.0f) {
+        yPos = -1.0f;
+    }
+    
+    glutPostRedisplay(); // 重新渲染
+}
+
+#pragma mark - main 函数，初始化工作
 int main(int argc,char* argv[])
 
 {
@@ -111,6 +225,11 @@ int main(int argc,char* argv[])
     glutReshapeFunc(ChangeSize);
     
     glutDisplayFunc(RenderScene);
+    
+    
+//    glutSpecialFunc(SpeacialKeysRect); // 通过坐标改变位置
+    glutSpecialFunc(SpeacialKeysMatrix); // 通过矩阵改变位置
+
     
     //驱动程序的初始化中没有出现任何问题。
     
